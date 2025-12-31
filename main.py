4bf74eef2b4b4463b9733a911ac1f294acafafa9
Sachin <rollercoster246@gmail.com>
2025-12-31 14:19:18 +0530

    tts first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from shabda-tts!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ mel-spectrogram/mel.py
@@ -0,0 +1,8 @@
+import librosa
+import librosa.display
+import numpy as np 
+import matplotlib.pyplot as plt
+import soundfile as sf
+
+
+print("Mel-spectrogram module imported successfully.")
